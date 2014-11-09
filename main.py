d0e5b68fa2f9e6ecd09c3dfadbb41bb470fba55e
Nahom Workie <nahomh@Nahoms-MacBook-Pro.local>
2014-11-09 15:45:39 -0800

    adding db layer

--- main.py
+++ main.py
@@ -7,14 +7,44 @@ from datetime import datetime
 from flask.ext.wtf import Form #class form
 from wtforms import StringField, SubmitField #fields 
 from wtforms.validators import Required #param required
+from flask.ext.sqlalchemy import SQLAlchemy 
+
+
+#adding data abstraction layer using sqlalchemy 
+basedir = os.path.abspath(os.path.dirname(__file__)) #db location
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+ 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'this_is_a_test_string' #CSRF protection --> should be stored in an enviornment variable rather than embedded in the actual code
 
-manager=Manager(app)
-bootstrap=Bootstrap(app)
-moment=Moment(app)
+manager=Manager(app) #initiate manager for server
+bootstrap=Bootstrap(app) #initiate bootstrap for styling
+moment=Moment(app) #initiate moment for time
+db=SQLAlchemy(app) #initiate db
+
+
+#models for db
+class Role(db.Model):
+	__tablename__='roles'
+	id=db.Column(db.Integer, primary_key=True)
+	name=db.Column(db.String(64), unique=True)
+
+	def __repr_(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__='users'
+	id=db.Column(db.Integer, primary_key=True)
+	username=db.Column(db.String(64),unique=True,index=True)
+
+	def __repr__(self):
+ 		return '<User %r>' % self.username
+
+
 
 #Forms
 class NameForm(Form):
@@ -52,11 +82,11 @@ def user(name):
 	return render_template('user.html', name=name)
 
 
-#custom error handeling 
-
+#custom routes for error handeling 
 @app.errorhandler(404)
 def page_not_found(e):
  return render_template('404.html'), 404
+
 @app.errorhandler(500)
 def internal_server_error(e):
  return render_template('500.html'), 500
