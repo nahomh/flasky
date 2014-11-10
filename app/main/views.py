4a3fd7455de1534a913c753943671e90e34e8e34
Nahom Workie <nahomh@Nahoms-MacBook-Pro.local>
2014-11-09 21:44:59 -0800

    working app complete

--- app/main/views.py
+++ app/main/views.py
@@ -4,8 +4,6 @@ from ..models import User
 from ..email import send_email
 from . import main
 from .forms import NameForm
-from datetime import datetime
-
 
 
 @main.route('/', methods=['GET', 'POST'])
