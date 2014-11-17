79d27ec7765a3fa7056f729369f915de5452ae96
Nahom Workie <nahomh@Nahoms-MacBook-Pro.local>
2014-11-16 16:28:07 -0800

    registeration

--- app/__init__.py
+++ app/__init__.py
@@ -11,13 +11,11 @@ mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
-#logging
-login_manager=LoginManager()
-login_manager.session_protection='strong'
-login_manager.login_view='auth.login'
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
-#Factory function
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
@@ -29,13 +27,10 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
-    #this is still missing the routing and custom error -> Blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    #registering auth blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
 
     return app
